164a7a04065e911716a275abe7f60c2af3bbbe69
Moacyr-Soares <soares.moacyr.r@gmail>
2024-09-27 11:37:30 -0300

    Starting challenge

---
+++ domain-model-and-orm-challenge/src/main/java/com/moacyr_soares/domain_model_and_orm_challenge/DomainModelAndOrmChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.moacyr_soares.domain_model_and_orm_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainModelAndOrmChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainModelAndOrmChallengeApplication.class, args);
+	}
+
+}
---
+++ domain-model-and-orm-challenge/src/test/java/com/moacyr_soares/domain_model_and_orm_challenge/DomainModelAndOrmChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moacyr_soares.domain_model_and_orm_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainModelAndOrmChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
