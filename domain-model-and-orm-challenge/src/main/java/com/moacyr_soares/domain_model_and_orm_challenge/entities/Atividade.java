a54c4129ed8d2172643f5c3d4f85b0af63d1f342
Moacyr-Soares <soares.moacyr.r@gmail>
2024-09-27 15:12:48 -0300

    Participante

--- domain-model-and-orm-challenge/src/main/java/com/moacyr_soares/domain_model_and_orm_challenge/entities/Atividade.java
+++ domain-model-and-orm-challenge/src/main/java/com/moacyr_soares/domain_model_and_orm_challenge/entities/Atividade.java
@@ -2,6 +2,11 @@ package com.moacyr_soares.domain_model_and_orm_challenge.entities;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Entity
 @Table(name = "tb_atividade")
 public class Atividade {
@@ -10,9 +15,15 @@ public class Atividade {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+
+    @Column(columnDefinition = "TEXT")
     private String descricao;
     private Double preco;
 
+    @ManyToMany(mappedBy = "atividades")
+    private Set<Participante> participantes = new HashSet<>();
+    @OneToMany(mappedBy = "atividade")
+    private List<Bloco> blocos = new ArrayList<>();
     @ManyToOne
     @JoinColumn(name = "categoria_id")
     private Categoria categoria;
@@ -20,11 +31,12 @@ public class Atividade {
     public Atividade(){
 
     }
-    public Atividade(Integer id, String nome, String descricao, Double preco) {
+    public Atividade(Integer id, String nome, String descricao, Double preco, Categoria categoria) {
         this.id = id;
         this.nome = nome;
         this.descricao = descricao;
         this.preco = preco;
+        this.categoria = categoria;
     }
 
     public Integer getId() {
@@ -57,5 +69,28 @@ public class Atividade {
 
     public void setPreco(Double preco) {
         this.preco = preco;
+    }
+    public Set<Participante> getParticipantes() {
+        return participantes;
+    }
+
+    public void setParticipantes(Set<Participante> participantes) {
+        this.participantes = participantes;
+    }
+
+    public List<Bloco> getBlocos() {
+        return blocos;
+    }
+
+    public void setBlocos(List<Bloco> blocos) {
+        this.blocos = blocos;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
     }
 }
